7f3c65ac34a88d94af1d5943e77b9c737564851e
Quinn Gideon <Quinn.Gideon@gmail.com>
2016-04-21 16:03:06 -0400

    styling home page and adding candy ipsum

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
 
-get '/' do
+get '/Home' do
 	erb :index
+end
+
+
+get '/Products' do
+	erb :products
+end
+
+get '/Locations' do
+	erb :locations
+end
+
+get '/Contact' do
+	erb :contact
 end
